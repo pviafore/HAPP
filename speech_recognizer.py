cc7ad21d0465710e335532676db58ceb9c70bcda
unknown <patviafore@gmail.com>
2015-03-27 22:11:27 -0500

    Tweaking sensitivity parameters

--- speech_recognizer.py
+++ speech_recognizer.py
@@ -3,8 +3,9 @@ import speech_recognition as sr
  
 def get_spoken_phrases():
     r = sr.Recognizer()
-    r.pause_threshold = 0.3
+    r.pause_threshold = 0.5
     r.quiet_threshold = 0.1
+    r.energy_threshold = 50
     while True:
         with sr.Microphone() as source:                # use the default microphone as the audio source
             audio = r.listen(source)
