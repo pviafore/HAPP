74cd464f60b686af62fae54bd609f5a40db3cd0c
Pat Viafore <patviafore@gmail.com>
2016-05-22 15:54:43 -0500

    Updated Weather Handler to use Wunderground API

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ This is a project for Hack Huntsville's first Hack-a-thon
 For the hack-a-thon, the automation is going to take care of voice-controlling certain websites, 
 with the first goal being Pandora (play, stop, next).
 
-This uses speech_recognition and pyaudio, as well as selenium
+This uses speech_recognition and pyaudio, as well as selenium, pyttsx
 
 License: MIT License
--- weather_handler.py
+++ weather_handler.py
@@ -1,23 +1,30 @@
-from selenium import webdriver
+import requests
 import pyttsx
+import ConfigParser
+from selenium import webdriver
 class WeatherHandler:
     def __init__(self):
         self.driver = None
-        
+        self.config = ConfigParser.ConfigParser()
+        self.config.read("config/wunderground.config")
+
     def display_waff(self):
         self.driver = webdriver.Firefox()
         self.driver.get("http://waff.com")
-    
+
     def exit_browser(self):
         self.driver.close()
         self.driver.quit()
-        
+
+    def get_config_data(self):
+        return [self.config.get("Weather Underground", config) for config in ["key", "state", "city"]]
+
+    def get_weather(self):
+        config_data = self.get_config_data()
+        r = requests.get("http://api.wunderground.com/api/{}/conditions/q/{}/{}.json".format(*config_data))
+        return r.json()["current_observation"]['temp_f']
+
     def speak_current_temperature(self):
-        tempDriver = webdriver.PhantomJS(executable_path="phantomjs.exe")
-        tempDriver.get("http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=35758")
-        weather = tempDriver.find_element_by_id("curTemp").find_element_by_class_name("wx-value").text
         text_to_speech = pyttsx.init()
-        text_to_speech.say("The current temperature is {} degrees Farenheit".format(weather))
+        text_to_speech.say("The current temperature is {} degrees Farenheit".format(self.get_weather()))
         text_to_speech.runAndWait()
-        tempDriver.close()
-        tempDriver.quit()
